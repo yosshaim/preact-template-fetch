1532a59a5fb4d1be560f0c183221ae9dfc5140ef
KESHET_BROAD\yosi.haim <yosi.haim@mako.co.il>
2018-01-17 12:27:08 +0200

    first commit

---
+++ template/src/fetch.js
@@ -0,0 +1,56 @@
+import { h, Component } from 'preact';
+export default class Fetch extends Component {
+    constructor(props) {
+        super(props);
+  
+        this.state = {
+            data: [],
+            isLoading: true
+        }
+    }
+  
+    componentDidMount() {
+       
+       var host=location.host
+        fetch(`//${host}/ajaxPage?jspName=${this.props.url}`).then((response)=>{
+			return response.json()
+		}).then((data)=>{
+            console.log(data)
+			this.setState({data:data,isLoading:false})
+		}).catch((err)=>{
+            console.log(err)
+            this.setState({isLoading:false})
+        })
+
+
+       
+       
+    }
+    render(){
+        const {data, isLoading} = this.state;
+       
+    
+        if(isLoading){
+          return <p>טוען ...</p>
+        }
+    
+        return (
+          <ul className={this.props.containerClassName}>
+            {data.map(item =>
+              <li key={item.id}>
+                <figure>
+                    <a href={item.url}><img src={item.pic}/></a>
+                    <figcaption>
+                        <a href={item.url}>
+                            <strong>{item.title}</strong>
+                            <small>{item.subtitle}</small>
+                        </a>
+                    </figcaption>
+                </figure>
+              </li>
+            )}
+          </ul>
+        );
+    }
+}
+ 
---
+++ template/src/index.js
@@ -0,0 +1,8 @@
+import { h, Component } from 'preact';
+import Fetch from './fetch.js';
+
+
+render(
+   <Fetch url="search_results_main_response.jsp&searchstring_input=%D7%A0%D7%A1%D7%9C%D7%99+%D7%91%D7%A8%D7%93%D7%94&page=1&tab=search_results_tab_general&searchString=%25D7%25A0%25D7%25A1%25D7%259C%25D7%2599%2B%25D7%2591%25D7%25A8%25D7%2593%25D7%2594&channelId=3d385dd2dd5d4110VgnVCM100000290c10acRCRD&vgnextoid=3d385dd2dd5d4110VgnVCM100000290c10acRCRD" 
+            containerClassName="preact-fetch"
+   />,document.body);
